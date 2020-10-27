def64d19e9264f0043ea5eb91483533c84c86bf8
Cam <Cam@CamzBeast>
2020-10-27 14:28:18 +1300

    first day version by Cameron test

---
+++ src/physicscalcappgui/PhysicsCalcAppGUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physicscalcappgui;
+
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Cam
+ */
+public class PhysicsCalcAppGUI {
+    
+    
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
